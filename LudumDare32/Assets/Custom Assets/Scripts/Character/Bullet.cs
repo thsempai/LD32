0f76f7e1d4c61b653822611417894d783b6c187c
Thomas Stassin <thomas.stassin@gsmproductions.com>
2015-04-18 14:50:34 +0200

    gun

---
+++ LudumDare32/Assets/Custom Assets/Scripts/Character/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        float x = transform.position.x;
+        float y = transform.position.y;
+        float z = transform.position.z;
+
+        x = x + Time.deltaTime * speed;
+
+        transform.position = new Vector3(x,y,z);
+    }
+}
--- LudumDare32/Assets/Custom Assets/Scripts/Character/Gun.cs
+++ LudumDare32/Assets/Custom Assets/Scripts/Character/Gun.cs
@@ -1,15 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class Gun : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    const string BULLET_PREFAB = "bullet";
+
+    public float bulletSpeed = 1f;
+    public Vector2 delta;
+
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyUp(KeyCode.N)) {
+            Shoot();
+        }
+    }
+
+    private void Shoot() {
+        GameObject bullet =
+            Instantiate(Resources.Load(BULLET_PREFAB, typeof(GameObject))) as GameObject;
+
+
+        float sens = 1f;
+        if (transform.localScale.x < 0) {
+            sens = -1f;
+        }
+
+        float x = transform.position.x + delta.x * sens;
+        float y = transform.position.y + delta.y;
+        float z = transform.position.z;
+
+        bullet.transform.position = new Vector3(x,y,z);
+        bullet.GetComponent<Bullet>().speed = bulletSpeed * sens;
+    }
 }
