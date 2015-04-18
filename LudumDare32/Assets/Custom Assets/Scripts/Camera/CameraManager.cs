0cc90ea597a5da8885e7f67a6878100feacc33c6
Thomas Stassin <thomas.stassin@gsmproductions.com>
2015-04-18 13:13:37 +0200

    camera logique

--- LudumDare32/Assets/Custom Assets/Scripts/Camera/CameraManager.cs
+++ LudumDare32/Assets/Custom Assets/Scripts/Camera/CameraManager.cs
@@ -1,15 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+
 public class CameraManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    private TimeManager manager;
+
+    public GameObject cameraPresent;
+    public GameObject cameraPast;
+
+    public void SetManager(TimeManager manager) {
+        this.manager = manager;
+    }
+
+    public void ChangeTime() {
+        switch(manager.currentTime) {
+            case TimeManager.Time.present : 
+                cameraPresent.SetActive(true);
+                cameraPast.SetActive(false);
+                break;
+                
+            case TimeManager.Time.past :
+                cameraPresent.SetActive(false);
+                cameraPast.SetActive(true);
+                break;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+
 }
---
+++ LudumDare32/Assets/Custom Assets/Scripts/TimeManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TimeManager : MonoBehaviour {
+
+    public const int PRESENT_LAYER = 9;
+    public const int PAST_LAYER = 10;
+
+    public enum Time{
+        past, 
+        present
+    }
+
+    public CameraManager cameraManager;
+    public GameObject hero;
+
+    private Time _currentTime = Time.present;
+
+    public Time currentTime{
+        get { return _currentTime;
+
+        }
+        set {
+            ChangeTime(value);
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        
+        cameraManager.SetManager(this);
+        hero.layer = PRESENT_LAYER;
+
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    if (Input.GetKeyUp(KeyCode.B)) {
+        if(currentTime == Time.present) {
+            currentTime = Time.past;
+        }
+        else {
+            currentTime = Time.present;
+        }
+    }
+    }
+
+    private void ChangeTime(Time newTime) {
+        _currentTime = newTime;
+
+        switch(_currentTime) {
+            case Time.present :
+                hero.layer = PRESENT_LAYER;
+                break;
+                
+            case Time.past : 
+                hero.layer = PAST_LAYER;
+                break;
+        }
+
+        cameraManager.ChangeTime();
+    }
+}
