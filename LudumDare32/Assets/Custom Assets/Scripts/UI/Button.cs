792a109e418c54d224a3cc11537df235ece26a70
Thomas Stassin <thomas.stassin@gsmproductions.com>
2015-04-19 10:35:33 +0200

    screen title (draft)

---
+++ LudumDare32/Assets/Custom Assets/Scripts/UI/Button.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+
+    public void Load(string name) {
+        Application.LoadLevel(name);
+    }
+}
